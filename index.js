55735d3e6ea988a4baa9791c590fa07c64f5a406
Thoams Krg <thoams.krieg@gmail.com>
2024-02-17 16:29:55 +0100

    first comit premières notions du js

---
+++ index.js
@@ -0,0 +1,81 @@
+// SELECTEURS
+// document.querySelector("h4").style.background = "yellow";
+// const baliseHtml = document.querySelector("h4");
+
+// Click Event
+const questionContainer = document.querySelector(".click-event");
+const btn1 = document.querySelector("#btn-1");
+const btn2 = document.getElementById("btn-2");
+const response = document.querySelector("p");
+
+questionContainer.addEventListener("click", () => {
+  questionContainer.classList.toggle("question-clicked");
+});
+
+btn1.addEventListener("click", () => {
+  response.classList.add("show-response");
+  response.style.background = "green";
+});
+
+btn2.addEventListener("click", () => {
+  console.log("Click !");
+  response.classList.add("show-response");
+  response.style.background = "red";
+});
+
+// ---------------------------------------------
+
+// Mouse Events
+const mousemove = document.querySelector(".mousemove");
+
+window.addEventListener("mousemove", (e) => {
+  mousemove.style.left = e.pageX + "px";
+  mousemove.style.top = e.pageY + "px";
+});
+
+window.addEventListener("mousedown", () => {
+  mousemove.style.transform = "scale(2) translate(-25%, -25%)";
+});
+
+window.addEventListener("mouseup", () => {
+  mousemove.style.transform = "scale(1) translate(-50%, -50%)";
+  mousemove.style.border = "2px solid teal";
+});
+
+questionContainer.addEventListener("mouseenter", () => {
+  questionContainer.style.background = "rgba(0,0,0,0.6)";
+});
+
+questionContainer.addEventListener("mouseout", () => {
+  questionContainer.style.background = "pink";
+});
+
+response.addEventListener("mouseover", () => {
+  response.style.transform = "rotate(2deg";
+});
+
+// ---------------------------------------------------
+// KeyPress event
+const keypresscontainer = document.querySelector(".keypress");
+const key = document.getElementById("key");
+
+const ring = (key) => {
+  const audio = new Audio();
+  audio.src = key + ".mp3";
+  audio.play();
+};
+
+document.addEventListener("keypress", (e) => {
+  key.textContent = e.key;
+
+  if (e.key === "j") {
+    keypresscontainer.style.background = "pink";
+  } else if (e.key === "h") {
+    keypresscontainer.style.background = "teal";
+  } else {
+    keypresscontainer.style.background = "red";
+  }
+
+  ring(e.key);
+});
+
